a5021e64c310829c5f45250c860b41775a812a4b
shubham <shubham@getsafle.com>
2022-04-06 12:07:31 +0530

    Updated methods to get tranactions on binance smart chain

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,2 @@
-module.exports.networks = ['ropsten', 'rinkeby', 'kovan', 'goerli', 'mainnet', 'polygon-mainnet'];
+module.exports.networks = ['ropsten', 'rinkeby', 'kovan', 'goerli', 'mainnet', 'polygon-mainnet', 'bsc-mainnet'];
 
